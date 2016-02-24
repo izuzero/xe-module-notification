8439db04319e526a3415946ab53d9f7042a523bd
izuzero <izuzero@gmail.com>
2016-02-24 20:47:16 +0900

    Admin Index 페이지 추가

--- notification.admin.view.php
+++ notification.admin.view.php
@@ -3,6 +3,20 @@
 
 class notificationAdminView extends notification
 {
+	public function init()
+	{
+		$this->__setTplPath();
+	}
+
+	public function dispNotificationAdminPlugins()
+	{
+	}
+
+	protected function __setTplPath()
+	{
+		$this->setTemplatePath($this->module_path . 'tpl');
+		$this->setTemplateFile(lcfirst(str_replace('dispNotificationAdmin', '', $this->act)));
+	}
 }
 
 /* End of file notification.admin.view.php */
